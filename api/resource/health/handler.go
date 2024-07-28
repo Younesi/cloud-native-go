60e9791bab85bd9688f18d1130ba1ac6fefb7722
Mahdi Younesi <mehdi.younesi7@gmail.com>
2024-07-28 21:37:42 +0200

    add repository

--- api/resource/health/handler.go
+++ api/resource/health/handler.go
@@ -4,9 +4,11 @@ import "net/http"
 
 // Read godoc
 //
-//  @summary        Read health
-//  @description    Read health
-//  @tags           health
-//  @success        200
-//  @router         /../livez [get]
-func Read(w http.ResponseWriter, r *http.Request) {}
+//	@summary        Read health
+//	@description    Read health
+//	@tags           health
+//	@success        200
+//	@router         /../livez [get]
+func Read(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("It is working"))
+}
